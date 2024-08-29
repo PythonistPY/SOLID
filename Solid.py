d0dec0cf29d5e6928a5a61e637d6db7e9556fe29
Kimi Ktoiani <kimo.tiens@gmail.com>
2024-08-29 13:12:27 +0400

    update main

---
+++ Solid.py
@@ -0,0 +1,35 @@
+# from abc import ABC, abstractmethod
+#
+# class Weapon(ABC):
+#     @abstractmethod
+#     def attack(self):
+#         pass
+#
+# class Sword(Weapon):
+#     def __init__(self,attack):
+#         self.attack()
+#         print(f"Я атакую {attack}")
+#
+#
+# class Bow(Weapon):
+#     def __init__(self,attack):
+#         self.attack()
+#         print(f"Я атакую {attack}")
+#
+#
+# class Fighter(Weapon):
+#     def __init__(self,weapon):
+#         pass
+#
+#
+#
+# class Monster():
+#     def __init__(self):
+#         pass
+#
+#
+#
+# fighter = Fighter
+# monster = Monster
+#
+
